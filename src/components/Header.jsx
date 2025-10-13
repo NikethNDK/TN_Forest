f8228bf57de6d3b257f7daa9be22d42f70c053fc
NikethNDK <nikethstudy@gmail.com>
2025-10-13 09:24:05 +0530

    header banner size reduced

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,6 +6,7 @@ const Header = () => {
             style={{
               backgroundImage: `url('https://images.unsplash.com/photo-1441974231531-c6227db76b6e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2071&q=80')`,
               backgroundSize: 'cover',
+              height: '50vh',
               backgroundPosition: 'center',
               backgroundRepeat: 'no-repeat'
             }}>
